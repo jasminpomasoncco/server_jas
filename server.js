00f8db2ed14d29011cfec741aacc3d036bb2346a
Jasmin <u20201b236@upc.edu.pe>
2022-07-14 21:36:35 -0500

    correccionexpress

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const router_server = require("./router/router_server");
 
 const app = express();
 const PORT = 8080;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
